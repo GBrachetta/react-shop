d0d651e4763dfd06d213ed8f13ae31d995d7f845
Guillermo Brachetta <brachetta@me.com>
2020-12-09 16:43:12 +0100

    Add Stripe integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51FFigNKZb5qTUkA0O38c6pEm9sgJ4YVEvJU05ZblVHcFy7SB7dMnEBQBm0sChReJxPUrNJXJO7evtfVoV8QUy6k900B35D0E3t";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="G Clothing"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
